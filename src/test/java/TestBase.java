4a433dec6e0a223ebcf3f300d25165bc29635ca7
haaktar <haaktar@gmail.com>
2022-01-21 22:29:18 -0500

    commit message

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class TestBase {
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import util.WebDriverFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+        driver = WebDriverFactory.getDriver("Chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+
+
+    }
+
 }
+
